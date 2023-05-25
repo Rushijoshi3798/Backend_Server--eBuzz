cc2d2ac3016521c5b2b9c80b928c4d8a73c72f32
Rushijoshi3798 <rushijoshi3798@gmail.com>
2023-05-25 21:02:44 +0530

    sorting functionality added in get route

--- routes/product.route.js
+++ routes/product.route.js
@@ -24,7 +24,7 @@ productRoute.post("/add", async (req, res) => {
     inStock,
     size,
     rating,
-    rating_count
+    rating_count,
   } = req.body;
 
   try {
@@ -41,9 +41,9 @@ productRoute.post("/add", async (req, res) => {
       description,
       size,
       rating_count,
-      rating
+      rating,
     });
-    console.log("Addproduct",AddProduct);
+    console.log("Addproduct", AddProduct);
     const saveProduct = await AddProduct.save();
     res.status(200).send({
       success: true,
@@ -52,7 +52,7 @@ productRoute.post("/add", async (req, res) => {
     });
   } catch (err) {
     console.log(err);
-    res.status(400).send({ success: false, message: "adfaff",err });
+    res.status(400).send({ success: false, message: "adfaff", err });
   }
 });
 
@@ -62,16 +62,33 @@ productRoute.get("/", async (req, res) => {
 
   const query = req.query;
   console.log("query", query);
-  // const {sort} = req.query
-  try {
-    // const sortOption =    sort ? { price: sort === 'asc' ? 1 : -1 } : {};
-    const products = await ProductModel.find(query);
-    res.status(200).send({
-      TotalCount: products.length,
-      products
-    });
-  } catch (error) {
-    res.status(400).send({ msg: "Something went wrong! " });
+
+  const { sort, order } = req.query;
+
+  if (sort && order) {
+    const sortOption = sort ? { discounted_price: sort === 'asc' ? 1 : -1 } : {};
+
+    try {
+      const products = await ProductModel.find().sort(sortOption);
+      res.status(200).send({
+        TotalCount: products.length,
+        products,
+      });
+    } catch (error) {
+      res.status(400).send({ msg: "Something went wrong!" });
+    }
+  } else {
+    // const {sort} = req.query
+    try {
+      // const sortOption = sort ? { price: sort === 'asc' ? 1 : -1 } : {};
+      const products = await ProductModel.find(query);
+      res.status(200).send({
+        TotalCount: products.length,
+        products,
+      });
+    } catch (error) {
+      res.status(400).send({ msg: "Something went wrong! " });
+    }
   }
 });
 
@@ -82,10 +99,10 @@ productRoute.get("/:id", async (req, res) => {
   // const {sort} = req.query
   try {
     // const sortOption =    sort ? { price: sort === 'asc' ? 1 : -1 } : {};
-    const products = await ProductModel.findOne({_id: id });
+    const products = await ProductModel.findOne({ _id: id });
     res.status(200).send({
       TotalCount: products.length,
-      products
+      products,
     });
   } catch (error) {
     res.status(400).send({ msg: "Something went wrong! " });
@@ -97,18 +114,20 @@ productRoute.patch("/update/:id", async (req, res) => {
   const payload = req.body;
 
   try {
-    const updateProduct = await ProductModel.findByIdAndUpdate(productId,payload)
+    const updateProduct = await ProductModel.findByIdAndUpdate(
+      productId,
+      payload
+    );
 
-    if(!updateProduct){
-      res.status(400).send({ error: 'Product not found' });
+    if (!updateProduct) {
+      res.status(400).send({ error: "Product not found" });
     }
 
     const sendUpdatedProduct = await updateProduct.save();
-    res.status(200).send({msg: 'Product Successfully Updated'})
-    
+    res.status(200).send({ msg: "Product Successfully Updated" });
   } catch (error) {
     console.log(error);
-    res.status(400).send({msg: "Something went Wrong"})
+    res.status(400).send({ msg: "Something went Wrong" });
   }
 });
 
@@ -116,16 +135,15 @@ productRoute.delete("/delete/:id", async (req, res) => {
   const productId = req.params.id;
 
   try {
-    const deleteProduct = await ProductModel.findByIdAndDelete(productId)
+    const deleteProduct = await ProductModel.findByIdAndDelete(productId);
 
-    if(!deleteProduct){
-      res.status(400).send({ error: 'Product not Deleted' });
+    if (!deleteProduct) {
+      res.status(400).send({ error: "Product not Deleted" });
     }
-    res.status(200).send({msg: 'Product Successfully Deleted'})
-
+    res.status(200).send({ msg: "Product Successfully Deleted" });
   } catch (error) {
     console.log(error);
-    res.status(400).send({msg: "Something went Wrong"})
+    res.status(400).send({ msg: "Something went Wrong" });
   }
 });
 
